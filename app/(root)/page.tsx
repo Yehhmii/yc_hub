d1b9a2a0e2b1f0e529dc565a51f9fa5b32cb283a
Yehhmii <franciscoabhulimen@gmail.com>
2025-05-04 17:49:54 -0600

    updated the hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
     <>
-      HOME
+    <section className="teal_container">
+      <h1 className="heading">Share your story <br /> Build and Evolve.</h1>
+      <p className="sub-heading !max-w-3xl">
+        Fast forward your growth, Vote and connect. <br />
+      </p>
+
+      <SearchForm />
+    </section>
     </>
   );
 }
---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Form from 'next/form'
+import SearchFormReset from './SearchFormReset';
+
+const SearchForm = () => {
+    const query = "Test";
+
+
+  return (
+    <Form action="/" scroll={false} className='search-form'>
+        <input 
+            name="query"
+            defaultValue={query}
+            placeholder="Search for a story, topic, or author"
+            className="search-input"
+        />
+
+        <div className='flex gap-2'>
+            {query && <SearchFormReset />}
+
+            <button type="submit" className='search-btn text-white'>
+                S
+            </button>
+        </div>
+    </Form>
+  )
+}
+
+export default SearchForm
